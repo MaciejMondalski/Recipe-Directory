8f11ba79b63276f0f8f45a8fdb27e750a6411c2a
MaciejMondalski <maciej.mondalski@gmail.com>
2022-02-03 19:03:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// page components
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
+import './App.scss';
+
+function App() {
+  return <div className='App'></div>;
+}
+
+export default App;
