b0522acb9e15b2440e00bc8fe25c91d705d43763
MaciejMondalski <maciej.mondalski@gmail.com>
2022-02-04 22:19:52 +0100

    first commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,24 @@
+import { useFetch } from '../../hooks/useFetch';
+import styled from 'styled-components';
+import RecipeList from '../../components/RecipeList';
+
 function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+
+  return (
+    <StyledHome>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </StyledHome>
+  );
 }
+
+const StyledHome = styled.div`
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export default Home;
