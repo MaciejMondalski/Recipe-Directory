669bc4cba4ecc6bc66942342393a3d8bb01cf39d
MaciejMondalski <maciej.mondalski@gmail.com>
2022-02-04 22:19:05 +0100

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
 
 // page components
 import Home from './pages/home/Home';
@@ -6,10 +7,41 @@ import Create from './pages/create/Create';
 import Recipe from './pages/recipe/Recipe';
 import Search from './pages/search/Search';
 
+// individual components
+import Navbar from './components/Navbar';
+
 import './App.scss';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div>
+      <StyledApp>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/create'>
+              <Create />
+            </Route>
+            <Route exact path='/search'>
+              <Search />
+            </Route>
+            <Route exact path='/recipes/:id'>
+              <Recipe />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </StyledApp>
+    </div>
+  );
 }
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default App;
