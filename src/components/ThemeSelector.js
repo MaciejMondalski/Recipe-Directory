820aad2a966ecc0215aaa9baaa78b16be76f41f1
MaciejMondalski <maciej.mondalski@gmail.com>
2022-02-16 23:29:59 +0100

    created mode toggle

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 import { useTheme } from '../hooks/useTheme';
+import modeIcon from '../assets/mode-icon.svg';
 
 const themeColors = ['#58249c', '#249c6b', '#b70233'];
 
 function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark');
+  };
+  console.log(mode);
 
   return (
     <StyledThemeSelector>
+      <div className='mode-toggle'>
+        <img
+          src={modeIcon}
+          onClick={toggleMode}
+          alt='dark/light toggle icon'
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+        />
+      </div>
       <div className='theme-buttons'>
         {themeColors.map((color) => (
           <div
@@ -35,6 +49,16 @@ const StyledThemeSelector = styled.div`
     cursor: pointer;
     margin-left: 15px;
     border-radius: 50%;
+  }
+
+  .mode-toggle {
+    margin-right: auto;
+
+    img {
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+    }
   }
 `;
 
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -6,21 +6,30 @@ const themeReducer = (state, action) => {
   switch (action.type) {
     case 'CHANGE_COLOR':
       return { ...state, color: action.payload };
+    case 'CHANGE_MODE':
+      return { ...state, mode: action.payload };
     default:
       return state;
   }
 };
 
 export function ThemeProvider({ children }) {
-  const [state, dispatch] = useReducer(themeReducer, { color: '#58249c' });
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: '#58249c',
+    mode: 'light',
+  });
 
   const changeColor = (color) => {
     dispatch({ type: 'CHANGE_COLOR', payload: color });
   };
 
+  const changeMode = (mode) => {
+    dispatch({ type: 'CHANGE_MODE', payload: mode });
+  };
+
   return (
     <div className=''>
-      <ThemeContext.Provider value={{ ...state, changeColor }}>
+      <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
         {children}
       </ThemeContext.Provider>
     </div>
