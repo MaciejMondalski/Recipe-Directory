6d9a2a96beff5f4170f36c2bc59b868f9dadf1e7
MaciejMondalski <maciej.mondalski@gmail.com>
2022-02-19 20:01:46 +0100

    added fetching data from firestore

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,37 @@
-import { useFetch } from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
 import styled from 'styled-components';
 import RecipeList from '../../components/RecipeList';
+import { useEffect, useState } from 'react';
 
 function Home() {
-  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection('recipes')
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError('No recipes to load');
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <StyledHome>
